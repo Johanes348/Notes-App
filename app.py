5ae4b59abde3769cc6074f3df2a763206004d12a
Sap Jolar <48847289-sappjolar@users.noreply.replit.com>
2025-10-22 14:09:44 +0000

    first commit

--- app.py
+++ app.py
@@ -6,24 +6,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Notes(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   note = db.Column(db.String(100), nullable=False)
-  date_created = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
+  date_created = db.Column(db.DateTime,
+                           default=datetime.datetime.now(datetime.UTC))
 
   def __init__(self, note):
     self.note = note
 
   def __repr__(self):
     return 'Id: %r' % self.id
-    
+
 
 # with app.app_context():
 #   db.create_all()
 
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
@@ -37,10 +41,12 @@ def add():
   except:
     return "Could not add note"
 
+
 @app.route('/notes')
 def view():
   notes = Notes.query.order_by(Notes.date_created).all()
   return render_template('notes.html', notes=notes)
 
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(debug=True)
