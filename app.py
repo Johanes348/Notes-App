4a6715b51beba53e28d5ab83290ac14696c8cf04
staoka24 <staoka24@student.loreto.ac.uk>
2025-10-22 19:47:54 +0100

    Added some more stuff

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 class Notes(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  note = db.Column(db.String(100), nullable=False)
+  note = db.Column(db.String(500), nullable=False)
   date_created = db.Column(db.DateTime,
                            default=datetime.datetime.now(datetime.UTC))
 
@@ -18,7 +18,6 @@ class Notes(db.Model):
 
   def __repr__(self):
     return 'Id: %r' % self.id
-
 
 # with app.app_context():
 #   db.create_all()
@@ -31,21 +30,53 @@ def index():
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
-  note = request.form['note']
-  new_note = Notes(note=note)
+  if (request.method == "POST"):
+    note = request.form['note']
+    new_note = Notes(note=note)
 
-  try:
-    db.session.add(new_note)
-    db.session.commit()
-    return redirect('/notes')
-  except:
-    return "Could not add note"
+    try:
+      db.session.add(new_note)
+      db.session.commit()
+      return redirect('/notes')
+    except:
+      return "Could not add note"
+  else:
+    return render_template('/index.html')
 
 
 @app.route('/notes')
 def view():
   notes = Notes.query.order_by(Notes.date_created).all()
   return render_template('notes.html', notes=notes)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  note_to_delete = Notes.query.get_or_404(id)
+  try:
+    db.session.delete(note_to_delete)
+    db.session.commit()
+    return redirect('/notes')
+  except:
+    return "Could not delete item"
+
+@app.route('/update/<int:id>', methods=["GET", "POST"])
+def update(id):
+  note_to_update = Notes.query.get_or_404(id)
+  return render_template('update.html', note=note_to_update)
+
+@app.route('/update-note/<int:id>', methods=["GET", "POST"])
+def update_note(id):
+  note_to_update = Notes.query.get_or_404(id)
+  updated_note = request.form['new-note']
+  note_to_update.note = updated_note
+    
+  try:
+    db.session.commit()
+  except:
+    return "Could not update"
+  return redirect('/notes')
+    
+    
 
 
 if __name__ == "__main__":
